79be7ae26bda67d06e200a223cf61e789dc0c7fa
Derek George <derek68038@gmail.com>
2015-01-29 21:51:46 -0600

    Finished Step 0, level 4(may need updated later)

--- rps.rb
+++ rps.rb
@@ -1,13 +1,35 @@
+# Class: Player
+#
+# Initializes the players names and sets their starting score to zero.
+#
+# Attributes:
+# @name   - String: the player's name.
+# @score  - Integer: Sets initial score to zero.
+
 class Player 
   
   attr_accessor :name, :score
     
   def initialize(name, score=0)
     @name = name
-    @score = score.to_i
+    @score = score
   end 
   
 end
+
+# Public: #game
+# Takes two arguments which create new Player objects.
+# Stores all the possible moves in an array.
+# Asks user the number of games they would like to play to the best of, which must be odd.
+# Sets each player's choice to a random element in the moves array.
+# Using a loop, it determines which player won each round, outputs the player's score 
+# total, and ends when a player has won the required number of games.
+#
+# Return:
+# String: Showing each round won, which lists each players moves and point totals.
+#
+# State Changes:
+# Sets player1 and player2 to the name's of the players.
 
 def game(p1, p2)
   player1 = Player.new(p1)
@@ -16,10 +38,11 @@ def game(p1, p2)
 
   puts "What number would you like to play to the best of? (number must be odd)?: "
   num = gets.chomp.to_i
-    until num.even? == false
-      puts "Your number must be odd. Try Again."
-      num = gets.chomp.to_i
-    end
+  
+  until num.even? == false
+    puts "Your number must be odd. Try Again."
+    num = gets.chomp.to_i
+  end
   
   best_of = num / 2.to_f
 
