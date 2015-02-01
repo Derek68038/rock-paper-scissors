f9738e6d90a2fd385e495f46420b7b898d898fe3
Derek George <derek68038@gmail.com>
2015-02-01 13:54:17 -0600

    Only un-commented some things. Finished Level 1, Step 1 for Player class.

--- rps_test_program.rb
+++ rps_test_program.rb
@@ -4,29 +4,29 @@ require 'minitest/autorun'
 
 class RPSTest < Minitest::Test
   
-  # def test_p1_moves_method_returns_string
-  #   test = Player.new("player1")
-  #   #assert_kind_of(String, test.p1_moves) #passes
-  #   assert_kind_of(Integer, test.p1_moves) #fails
-  # end
-  #
-  # def test_p2_moves_method_returns_string
-  #   test = Player.new("player1")
-  #   #assert_kind_of(String, test.p2_moves) #passes
-  #   assert_kind_of(Integer, test.p2_moves) #fails
-  # end
-  #
-  # def test_p1_win_method_returns_integer
-  #   test = Player.new("player1")
-  #   #assert_kind_of(String, test.p1_win) #fails
-  #   assert_kind_of(Integer, test.p1_win) #passes
-  # end
-  #
-  # def test_p2_win_method_returns_integer
-  #   test = Player.new("player1")
-  #   #assert_kind_of(String, test.p2_win) #fails
-  #   assert_kind_of(Integer, test.p2_win) #passes
-  # end
+  def test_p1_moves_method_returns_string
+    test = Player.new("player1")
+    #assert_kind_of(String, test.p1_moves) #passes
+    assert_kind_of(Integer, test.p1_moves) #fails
+  end
+
+  def test_p2_moves_method_returns_string
+    test = Player.new("player1")
+    #assert_kind_of(String, test.p2_moves) #passes
+    assert_kind_of(Integer, test.p2_moves) #fails
+  end
+
+  def test_p1_win_method_returns_integer
+    test = Player.new("player1")
+    #assert_kind_of(String, test.p1_win) #fails
+    assert_kind_of(Integer, test.p1_win) #passes
+  end
+
+  def test_p2_win_method_returns_integer
+    test = Player.new("player1")
+    #assert_kind_of(String, test.p2_win) #fails
+    assert_kind_of(Integer, test.p2_win) #passes
+  end
 
 end
 
