60e85f4001f38deb5d17b00a2b3c5dff82f1ce5b
Derek George <derek68038@gmail.com>
2015-01-31 19:08:11 -0600

    Completed Level 0, Step 4

--- rps_driver.rb
+++ rps_driver.rb
@@ -1,5 +1,17 @@
 require_relative 'rps_game'
   
+  # Public: #driver
+  # Creates the player's names, determines how many games they play to, starts the game loop, & states the winner.
+  #
+  # Parameters:
+  # None.
+  #
+  # Returns:
+  # The outcome of each round and who the overall winner is.
+  #
+  # State Changes:
+  # Sets @player1 and @player2's names.
+
   def driver
     start = Game.new("Derek", "Hilary")
     start.play_to_n
